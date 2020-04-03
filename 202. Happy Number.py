88f0dffb35c23dfb71881f4062ce71b1fa952f92
Jitender Phogat <41241360+jitender18@users.noreply.github.com>
2020-04-02 21:11:03 -0400

    happy number updated

--- 202. Happy Number.py
+++ 202. Happy Number.py
@@ -21,3 +21,29 @@ class Solution:
             seen.add(n)
             n = sum(int(i) ** 2 for i in str(n))
         return 1 in seen
+
+
+#############################################
+############## Other Solutions ##############
+#############################################
+
+
+ # Time:  O(k), where k is the steps to be happy number
+# Space: O(k)
+
+class Solution(object):
+    # @param {integer} n
+    # @return {boolean}
+    def isHappy(self, n):
+        lookup = {}
+        while n != 1 and n not in lookup:
+            lookup[n] = True
+            n = self.nextNumber(n)
+        return n == 1
+
+    def nextNumber(self, n):
+        new = 0
+        for char in str(n):
+            new += int(char)**2
+        return new
+
