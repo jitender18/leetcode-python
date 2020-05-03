8f220e5ee8db58c9ffd322c82ab5f8d0e0758ea0
Jitender Phogat <41241360+jitender18@users.noreply.github.com>
2020-05-02 22:05:56 -0400

    Valid Parenthesis String

---
+++ Valid Parenthesis String.py
@@ -0,0 +1,66 @@
+# Given a string containing only three types of characters: '(', ')' and '*',
+# write a function to check whether this string is valid.
+# We define the validity of a string by these rules:
+
+# Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+# Any right parenthesis ')' must have a corresponding left parenthesis '('.
+# Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+# '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
+# An empty string is also valid.
+
+# Example 1:
+# Input: "()"
+# Output: True
+# Example 2:
+# Input: "(*)"
+# Output: True
+# Example 3:
+# Input: "(*))"
+# Output: True
+# Note:
+# The string size will be in the range [1, 100].
+
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        cnt = 0
+        for ch in s:
+            if ch == "(" or ch == "*":
+                cnt += 1
+            else:
+                cnt -= 1
+                if cnt < 0:
+                    return False
+        if cnt == 0:
+            return True
+        
+        cnt = 0
+        for ch in s[::-1]:
+            if ch == ")" or ch == "*":
+                cnt += 1
+            else:
+                cnt -= 1
+                if cnt < 0:
+                    return False
+        return True
+
+
+
+
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def checkValidString(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        lower, upper = 0, 0  # keep lower bound and upper bound of '(' counts
+        for c in s:
+            lower += 1 if c == '(' else -1
+            upper -= 1 if c == ')' else -1
+            if upper < 0: break
+            lower = max(lower, 0)
+        return lower == 0  # range of '(' count is valid
+
